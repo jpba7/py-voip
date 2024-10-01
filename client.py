4cb81e283ca57ee62ab3feaa79280ffe05c3c3bc
João Pedro Bimbato Araujo <nhcsfl@gmail.com>
2024-10-01 18:48:20 -0300

    chore: comments and organization

--- client.py
+++ client.py
@@ -1,55 +1,64 @@
+import pyaudio
+import socket
 import sys
 import time
-
-import socket
-import pyaudio
-import wave
-from tkinter import *
 import threading
+from tkinter import *
 
-# Audio Stream (PyAudio) Initialization
-CHUNK = 1024
-FORMAT = pyaudio.paInt16 # Change to 8 if doesn't work
-CHANNELS = 2
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "output.wav"
-RATE = 44100
-p = pyaudio.PyAudio()
-stream = p.open(format = FORMAT,
-                channels = CHANNELS,
-                rate = RATE,
-                input = True,
-                frames_per_buffer = CHUNK)
+chunk = 1024 
+FORMAT = pyaudio.paInt16  
+CHANNELS = 1  
+RATE = 10240
 
-# Socket Initialization
-size = 1024
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(),1234))
+p = pyaudio.PyAudio() # Cria um objeto pyaudio
 
-# GUI Specifics
-class VOIP_FRAME(Frame):    
+# Cria um stream de audio
+stream = p.open(format = FORMAT,            # Formato do audio (16 bits por amostra)
+                channels = CHANNELS,        # Numero de canais (1 = mono, 2 = stereo)       
+                rate = RATE,                # Taxa de amostragem em Hz
+                input = True,               # Indica que é para gravar o audio
+                frames_per_buffer = chunk)  # Tamanho do buffer de audio
+
+# Inicialização do socket
+host = 'localhost'
+port = 50000
+size = 1024 # Tamanho do buffer
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Criação do socket (AF_INET = IPv4, SOCK_DGRAM = UDP)
+s.connect((host,port)) # Conecta o socket com o servidor específicado
+
+class VOIP_FRAME(Frame):
+    
     def OnMouseDown(self, event):
+        # Evento de clique do mouse, seta a variavel self.mute para False e chama a função speakStart
         self.mute = False
-        self.speakStart()        
-    def muteSpeak(self,event):
+        self.speakStart()
+        
+    def muteSpeak(self, event):
+        # Quando o botão é solto, a variavel self.mute é setada para True
         self.mute = True
-        print("You are now muted")
+        print("Você foi mutado")
+        
     def speakStart(self):
+        # Inicializa a thread que irá enviar os dados do microfone para o servidor
         t = threading.Thread(target=self.speak)
-        t.start()                
+        t.start()
+                
     def speak(self):
-        print("You are now speaking")
+        print("Você está desmutado")
         while self.mute is False:
-            data = stream.read(CHUNK)
-            print(data)
+            data = stream.read(chunk)
             s.send(data)
             s.recv(size)
+        
+
     def createWidgets(self):
         self.speakb = Button(self)
-        self.speakb["text"] = "Speak"
+        self.speakb["text"] = "Falar"
         self.speakb.pack({"side": "left"})
+
         self.speakb.bind("<ButtonPress-1>", self.OnMouseDown)
         self.speakb.bind("<ButtonRelease-1>", self.muteSpeak)
+
     def __init__(self, master=None):
         self.mute = True
         Frame.__init__(self, master)
@@ -64,3 +73,5 @@ root.destroy()
 s.close()
 stream.close()
 p.terminate()
+
+    
--- server.py
+++ server.py
@@ -2,33 +2,36 @@
 import pyaudio
 import socket
 import sys
-import socketserver
 
 # Pyaudio Initialization
 chunk = 1024
 pa = pyaudio.PyAudio()
 
 # Opening of the audio stream
-stream = pa.open(format = 8,
+stream = pa.open(format = pyaudio.paInt16,
                 channels = 1,
                 rate = 10240,
                 output = True)
 
 # Socket Initialization
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)     # For using same port again
-s.bind((socket.gethostname(), 1234))
+host = 'localhost'
+port = 50000
+backlog = 5
 size = 1024
-s.listen(5)
-client, address = s.accept()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((host,port))
+sock.listen(backlog)
 
+client, address = sock.accept()
+print("Server is now running\n=======================")
 
-print('Server Running...')
+# Main Functionality
 while 1:
     data = client.recv(size)
     if data:
+        # Write data to pyaudio stream
         stream.write(data)  # Stream the recieved audio data
-        client.send(b'ACK')  # Send back Acknowledgement, has to be in binary form
+        client.send('ACK')  # Send an ACK
 
 client.close()
 stream.close()
