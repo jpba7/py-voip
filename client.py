16e3a99083c48aa697594a027049772350585408
João Pedro Bimbato Araujo <nhcsfl@gmail.com>
2024-10-02 11:21:38 -0300

    chore: more docs

--- client.py
+++ client.py
@@ -7,68 +7,70 @@ from tkinter import *
 from pydub import AudioSegment
 from io import BytesIO
 
-chunk = 16384 
-FORMAT = pyaudio.paInt16  
-CHANNELS = 1  
+CHUNK = 16384
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
 RATE = 40960
 
-p = pyaudio.PyAudio() # Cria um objeto pyaudio
+p = pyaudio.PyAudio()  # Cria um objeto pyaudio
 
 # Cria um stream de audio
-stream = p.open(format = FORMAT,            # Formato do audio (16 bits por amostra)
-                channels = CHANNELS,        # Numero de canais (1 = mono, 2 = stereo)       
-                rate = RATE,                # Taxa de amostragem em Hz
-                input = True,               # Indica que é para gravar o audio
-                frames_per_buffer = chunk)  # Tamanho do buffer de audio
+stream = p.open(format=FORMAT,            # Formato do audio (16 bits por amostra)
+                channels=CHANNELS,        # Numero de canais (1 = mono, 2 = stereo)
+                rate=RATE,                # Taxa de amostragem em Hz
+                input=True,               # Indica que é para gravar o audio
+                frames_per_buffer=CHUNK)  # Tamanho do buffer de audio
 
 # Inicialização do socket
-host = socket.gethostname() # Pega o nome da máquina local
-port = 7777
-size = 32768 # Tamanho do buffer
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Criação do socket (AF_INET = IPv4, SOCK_DGRAM = UDP)
-s.connect((host,port)) # Conecta o socket com o servidor específicado
+HOST = socket.gethostname()                             # Pega o nome da máquina local
+PORT = 7777                                             # Porta
+SIZE = 32768                                            # Tamanho do buffer
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)    # Criação do socket (AF_INET = IPv4, SOCK_DGRAM = UDP)
+s.connect((HOST, PORT))                                 # Conecta o socket com o servidor específicado
 
-class VOIP_FRAME(Frame):
-    def OnMouseDown(self, event):
+
+class VoIPFrame(Frame):                                   # Classe que representa a janela principal
+    def on_mouse_down(self, event):                       # Função que é chamada quando o botão é pressionado
         self.mute = False
-        self.speakStart()
-        
-    def muteSpeak(self, event):
+        self.speak_start()
+
+    def mute_speak(self, event):                         # Função que é chamada quando o botão é solto (muta o microfone)
         self.mute = True
         print("Você foi mutado")
-        
-    def speakStart(self):
+
+    def speak_start(self):                               # Função que inicia a thread que envia o audio
         t = threading.Thread(target=self.speak)
         t.start()
-                
-    def speak(self):
+
+    def speak(self):                                      # Função que comprime e envia o audio
         print("Você está desmutado")
         while self.mute is False:
-            data = stream.read(chunk)
+            data = stream.read(CHUNK)                     # Lê o audio do microfone e armazena em data (dados brutos)
             audio_segment = AudioSegment(data, sample_width=2, frame_rate=RATE, channels=CHANNELS)
-            compressed_io = BytesIO()
-            audio_segment.export(compressed_io, format="mp3", bitrate="32k")
-            compressed_data = compressed_io.getvalue()
-            s.send(compressed_data)
-            s.recv(size)
-        
-    def createWidgets(self):
+            compressed_io = BytesIO()                     # Cria um buffer de bytes
+            audio_segment.export(compressed_io, format="mp3", bitrate="32k")  # Comprime o audio para MP3
+            compressed_data = compressed_io.getvalue()    # Pega os dados comprimidos do buffer de bytes
+            s.send(compressed_data)                       # Envia os dados comprimidos pro servidor
+            s.recv(SIZE)                                  # Recebe um ACK do servidor
+
+    def create_widgets(self):
         self.speakb = Button(self)
         self.speakb["text"] = "Falar"
         self.speakb.pack({"side": "left"})
 
-        self.speakb.bind("<ButtonPress-1>", self.OnMouseDown)
-        self.speakb.bind("<ButtonRelease-1>", self.muteSpeak)
+        self.speakb.bind("<ButtonPress-1>", self.on_mouse_down)
+        self.speakb.bind("<ButtonRelease-1>", self.mute_speak)
 
     def __init__(self, master=None):
         self.mute = True
         Frame.__init__(self, master)
         self.mouse_pressed = False
         self.pack()
-        self.createWidgets()
+        self.create_widgets()
+
 
 root = Tk()
-app = VOIP_FRAME(master=root)
+app = VoIPFrame(master=root)
 app.mainloop()
 root.destroy()
 s.close()
--- server.py
+++ server.py
@@ -5,34 +5,31 @@ import sys
 from pydub import AudioSegment
 from io import BytesIO
 
-# Pyaudio Initialization
-chunk = 16384
+CHUNK = 16384
 pa = pyaudio.PyAudio()
 
-# Opening of the audio stream
-stream = pa.open(format = pyaudio.paInt16,
-                channels = 1,
-                rate = 40960,
-                output = True)
+stream = pa.open(format=pyaudio.paInt16,    # Formato do audio (16 bits por amostra)
+                 channels=1,                # Numero de canais (1 = mono, 2 = stereo)
+                 rate=40960,                # Taxa de amostragem em Hz
+                 output=True)               # Indica que é pra reproduzir o audio
 
-# Socket Initialization
-host = socket.gethostname() # Pega o nome da máquina local
-port = 7777
-size = 32768
+# Inicialização do socket
+HOST = socket.gethostname()  # Pega o nome da máquina local
+PORT = 7777                  # Porta
+SIZE = 32768                 # Tamanho do buffer
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((host,port))
+sock.bind((HOST, PORT))      # Associa o socket com o endereço e porta especificados
 
 print("Server is now running\n=======================")
 
-# Funcionalidade principal
 try:
     while True:
-        data, addr = sock.recvfrom(size)
+        data, addr = sock.recvfrom(SIZE)            # Recebe os dados do cliente
         if data:
-            compressed_io = BytesIO(data)
-            audio_segment = AudioSegment.from_file(compressed_io, format="mp3")
-            stream.write(audio_segment.raw_data)  # Stream the received audio data
-            sock.sendto(b'ACK', addr)  # Send an ACK
+            compressed_io = BytesIO(data)           # Cria um buffer de bytes com os dados da memória
+            audio_segment = AudioSegment.from_file(compressed_io, format="mp3")  # Carrega o audio comprimido
+            stream.write(audio_segment.raw_data)    # Stream the received audio data
+            sock.sendto(b'ACK', addr)               # Manda o Ack
 except ConnectionResetError as e:
     print(f"Erro de conexão: {e}")
 finally:
